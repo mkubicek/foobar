96669d7f3dff8b66230b9f5aeccf075d8478e425
mkubicek <milankubicek@hotmail.com>
2022-08-14 22:59:34 +0200

    add solution for 2.2

---
+++ 2.2-Please-Pass-the-Coded-Messages/solution.py
@@ -0,0 +1,20 @@
+#
+# Naive solution for Please Pass the Coded Messages
+# Time O(n*log(n) + n! + (n-1)! + ..)
+# Space O(n)
+#
+import itertools
+
+def solution(digits):
+    digits.sort(reverse=True)
+    for r in range(len(digits), 1, -1):
+        for perm in itertools.combinations(digits, r):
+            if(sum(perm) % 3 == 0):
+                return int(''.join(map(str,perm)))
+    return 0
+
+if __name__ == "__main__":
+    print(solution([3, 1, 4, 1]))
+    print(solution([3, 1, 4, 1, 5, 9]))
+    assert solution([3, 1, 4, 1]) == 4311
+    assert solution([3, 1, 4, 1, 5, 9]) == 94311
