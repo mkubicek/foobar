f1484915c96f2a5f299f3100d0e76b30072e88c5
mkubicek <milankubicek@hotmail.com>
2022-09-04 11:45:34 +0200

    use numpy for solution 3.3

--- 3.3-Doomsday-Fuel/solution.py
+++ 3.3-Doomsday-Fuel/solution.py
@@ -1,100 +1,49 @@
 #
 # Solution for Doomsday Fuel:
-# 
-# Matrix multiplication code: https://www.programiz.com/python-programming/examples/multiply-matrix
-# Matrix inverse, gaussian elimination code: https://stackoverflow.com/a/61741074
+# Get probabilities for reaching absorbing states (final states).
 #
-from fractions import Fraction
+# Time O(n^3) -> np.linalg.det
+# Space O(n)
+#
+# Calculated using some absorbing markov chain linalg magic (https://en.wikipedia.org/wiki/Absorbing_Markov_chain):
+# 1) Divide probability matrix p in the form
+#    P= Q  R
+#       0  I
+# 2) Calculate 
+#    ((I_dim(Q) - Q) ^ -1) * R
+#    -> First row gives the probabilities for reaching absorbing states
 
-def eliminate(r1, r2, col, target=0):
-    fac = (r2[col]-target) / r1[col]
-    for i in range(len(r2)):
-        r2[i] -= fac * r1[i]
-
-def gauss(a):
-    for i in range(len(a)):
-        if a[i][i] == 0:
-            for j in range(i+1, len(a)):
-                if a[i][j] != 0:
-                    a[i], a[j] = a[j], a[i]
-                    break
-            else:
-                raise ValueError("Matrix is not invertible")
-        for j in range(i+1, len(a)):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)-1, -1, -1):
-        for j in range(i-1, -1, -1):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)):
-        eliminate(a[i], a[i], i, target=1)
-    return a
-
-def inverse(a):
-    tmp = [[] for _ in a]
-    for i,row in enumerate(a):
-        assert len(row) == len(a)
-        tmp[i].extend(row + [0]*i + [1] + [0]*(len(a)-i-1))
-    gauss(tmp)
-    ret = []
-    for i in range(len(tmp)):
-        ret.append(tmp[i][len(tmp[i])//2:])
-    return ret
-
-def multiply(X, Y):
-    return [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*Y)] for X_row in X]
+import numpy as np
 
 def solution(m):
-    denominators = [sum(x) for x in m]
-    absorbing_states = denominators.count(0)
+    # determine indexes for transient and absorbing states
     transient_states_idx = [idx for idx, state in enumerate(m) if sum(state) != 0]
     absorbing_states_idx = [idx for idx, state in enumerate(m) if sum(state) == 0]
-    
+
     # special case: state 0 is final state
     if sum(m[0]) == 0:
-        result = [1]
-        result.extend([0] * (absorbing_states - 1))
-        result.append(1)
-        return result
+        return [1] + [0] * (len(absorbing_states_idx) - 1) + [1]
 
-    # order m so that all absorbing states follow transient states
-    stateReorder = transient_states_idx
-    stateReorder.extend(absorbing_states_idx)
-    m2 = [[m[i][j] for j in stateReorder] for i in stateReorder]
-    denominators = [sum(x) for x in m2]
-    
-    Q = [x[:len(m2)-absorbing_states] for x in m2[:len(m2)-absorbing_states]]
-    R = [x[len(m2)-absorbing_states:] for x in m2[:len(m2)-absorbing_states]]
-    for i in range(len(Q)):
-        for j in range(len(Q[0])):
-            Q[i][j] = Fraction(Q[i][j], denominators[i])
-    for i in range(len(R)):
-        for j in range(len(R[0])):
-            R[i][j] = Fraction(R[i][j], denominators[i])
-    
-    # I - Q
-    for i in range(len(Q)):
-        for j in range(len(Q[0])):
-            if i == j:
-                Q[i][j] = Fraction(1, 1)
-            else:
-                Q[i][j] = Q[i][j] * -1
-    
-    P = multiply(inverse(Q), R)[0]
-
-    max_denominator = max([x.denominator for x in P])
-    common_denominator = max_denominator
-    for denominator in [x.denominator for x in P]:
-        if common_denominator % denominator != 0:
-            common_denominator = common_denominator * denominator
-            
-    for idx, prob in enumerate(P):
-        P[idx] = P[idx].numerator * (common_denominator // P[idx].denominator)
-    P.append(common_denominator)
-    
-    return P
+    # pack all transient state transition probabilities into numpy matrix 
+    m = np.matrix(m, dtype=float)[transient_states_idx, :]
+    # find common denominator by multiplying the denominators of each transition probability row
+    denominator = np.prod(m.sum(1))
+    # create proper probability matrix by dividing by denominator of each transition probability row
+    prob = m / m.sum(1)
+    # divide into Q and R
+    Q = prob[:, transient_states_idx]
+    R = prob[:, absorbing_states_idx]
+    I = np.identity(len(transient_states_idx))
+    N = (I - Q) ** (-1)
+    result = (N[0] * R * denominator / np.linalg.det(N))
+    result = result.round().astype(int).A1
+    gcd = np.gcd.reduce(result)
+    result = np.append(result, result.sum())
+    return (result / gcd).astype(int).tolist()
 
 if __name__ == "__main__":
-    print(solution([[0, 1, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
-    print(solution([[0, 0, 0, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
+    #print(solution([[0]]))
+    #print(solution([[0, 1, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
+    #print(solution([[0, 0, 0, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
     print(solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
     print(solution([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
